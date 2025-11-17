c3a99fe774ad62197fb3dedc24fbda86c4ecac27
Sunday Mapunda <sunnyjumapili@gmail.com>
2025-11-17 13:14:25 +0300

    flutter setup

---
+++ recircle/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(ReCircleApp());
+}
+
+class ReCircleApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ReCircle',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
