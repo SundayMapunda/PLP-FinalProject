87e70c63f268bced362ec70e18287a1572f663de
Sunday Mapunda <sunnyjumapili@gmail.com>
2025-11-17 12:58:55 +0300

    update settings.py

--- recircle-backend/recircle_backend/recircle_backend/settings.py
+++ recircle-backend/recircle_backend/recircle_backend/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -93,11 +97,11 @@ WSGI_APPLICATION = "recircle_backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "your_neon_db_name",  # Get from Neon connection string
-        "USER": "your_neon_user",
-        "PASSWORD": "your_neon_password",
-        "HOST": "ep-cool-breeze-123456.us-east-2.aws.neon.tech",  # Your Neon host
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME"),  # Get from Neon connection string
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),  # Your Neon host
+        "PORT": os.getenv("DB_PORT"),
         "OPTIONS": {"sslmode": "require"},
     }
 }
